127e657026488c870f1c7b52ae1f9878e29502ed
erthru <ersaka96@gmail.com>
2021-03-12 16:15:32 +0800

    tag schema done

---
+++ src/schemas/tag.ts
@@ -0,0 +1,24 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export enum TagDocument {
+    schemaName = "tag",
+    name = "name",
+}
+
+interface ITag extends Document {
+    [TagDocument.name]?: string;
+}
+
+const schema = new Schema(
+    {
+        [TagDocument.name]: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export default mongoose.model<ITag>(TagDocument.schemaName, schema);
