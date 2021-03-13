c28b7dbfb6867f836090e796209b0bf5f132a993
erthru <ersaka96@gmail.com>
2021-03-13 15:07:40 +0800

    search done

--- src/repositories/infographic-repository.ts
+++ src/repositories/infographic-repository.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { ERROR, OK } from "../helpers/json";
-import infographicSchema from "../schemas/infographic";
+import infographicSchema, { InfographicDocument } from "../schemas/infographic";
 
 export const getAll = async (req: Request, res: Response) => {
     try {
@@ -9,6 +9,30 @@ export const getAll = async (req: Request, res: Response) => {
 
         const infographics = await infographicSchema.find().sort({ _id: -1 }).skip(skip).limit(limit);
         const total = await infographicSchema.countDocuments();
+
+        OK(res, {
+            infographics: infographics,
+            total: total,
+        });
+    } catch (e: any) {
+        ERROR(res, e.message);
+    }
+};
+
+export const search = async (req: Request, res: Response) => {
+    try {
+        const limit = parseInt(req.query.limit as string);
+        const skip = (parseInt(req.query.page as string) - 1) * parseInt(req.query.limit as string);
+
+        const filter = {
+            [InfographicDocument.title]: {
+                $regex: req.query.q as string,
+                $options: "i",
+            },
+        };
+
+        const infographics = await infographicSchema.find(filter).sort({ _id: -1 }).skip(skip).limit(limit);
+        const total = await infographicSchema.countDocuments(filter);
 
         OK(res, {
             infographics: infographics,
--- src/routes.ts
+++ src/routes.ts
@@ -18,13 +18,14 @@ router.get("/dataset/slug/:slug", datasetRepository.getBySlug);
 router.put("/dataset/:id/increment-downloaded", datasetRepository.updateIncrementDownloaded);
 
 router.get("/visualizations", visualizationRepository.getAll);
-router.get("/visualizations/search/query", visualizationRepository.search)
+router.get("/visualizations/search/query", visualizationRepository.search);
 router.get("/visualization/slug/:slug", visualizationRepository.getBySlug);
 
 router.get("/organizations/include-datasets-total", organizationRepository.getAllWithDatasetsTotal);
 router.get("/organization/slug/:slug", organizationRepository.getBySlug);
 
 router.get("/infographics", infographicRepository.getAll);
+router.get("/infographics/search/query", infographicRepository.search);
 
 router.post("/seeder", seederRepository.add);
 
